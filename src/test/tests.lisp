64a75e6c67730007a002c3e61301bdd4689cfe28
Scott Adams <sradms0@protonmail.com>
2020-11-19 23:42:23 -0500

    clean and update testing

--- src/test/tests.lisp
+++ src/test/tests.lisp
@@ -1,13 +1,14 @@
 (load "../main/event.lisp")
 
-(setf item (make-instance 'Event))
-(setf (Event-Year item) 2021)
-(setf (Event-Month item) 11)
-(setf (Event-Day item) 10)
-(setf (Event-Hour item) 14)
-(setf (Event-Name item) "Class")
-(setf (Event-Notes item) "This is text filler")
-(setf (Event-Notes item) "This is text filler 2")
+(defun assert-equals (result expected)
+  (if (equal result expected)
+      (print "PASS")
+      (print "FAIL (results should be equal)")))
+
+(defun assert-not-equals (result expected)
+  (if (not (equal result expected))
+      (print "PASS")
+      (print "FAIL (results should not be equal)")))
 
 (setf eventOne (make-instance 'Event))
 (setf (Event-Year eventOne) 2022)
@@ -25,18 +26,27 @@
 (setf (Event-Name eventTwo) "School")
 (setf (Event-Notes eventTwo) "This is text filler")
 
-;;(getEvent item)
-(format t "Time Until Event Expected (True): ~d~%" (equal(timeUntil item 17) 21)) ;;Unit test One 
-;; (getYear item)
+(setf eventThree (make-instance 'Event))
+(setf (Event-Year eventThree) 2021)
+(setf (Event-Month eventThree) 11)
+(setf (Event-Day eventThree) 10)
+(setf (Event-Hour eventThree) 14)
+(setf (Event-Name eventThree) "Class")
+(setf (Event-Notes eventThree) "This is text filler")
+(setf (Event-Notes eventThree) "This is text filler 2")
 
 
-;;(getEvent eventOne)
-;;(format t "Time Until Event: ~d~%" (timeUntil eventOne 19)) ;;Unit test two
-(format t "Time Until Event Expected (True): ~d~%" (equal(timeUntil eventOne 19) 15)) ;;Unit test Two 
-(format t "Time Until Event Expected (False): ~d~%" (equal(timeUntil eventOne 19) 11)) ;;Unit test Two 
-;; (getYear eventOne)
+(assert-equals (getEvent eventOne)
+    (format t "Year: 2022\n"
+     "Month: 13\n"
+     "Day: 15\n" 
+     "Hour: 10\n" 
+     "Name: Birthday\n"
+     "Description/Notes: This is text filler"))
+(assert-equals (getYear eventOne) (format t "Year: 2022"))
+(assert-equals (getHour eventOne) (format t "Hour 10"))
 
-;;(getEvent eventTwo)
-;; (print(equal(timeUntil eventTwo 15) 14)) ;; T
-(format t "Time Until Event Expected (True): ~d~%" (equal(timeUntil eventTwo 15) 14)) ;;Unit test three 
-;; (getYear eventTwo)
+(assert-equals (timeUntil eventOne 19) 15)
+(assert-not-equals (timeUntil eventOne 19) 11)
+(assert-equals (timeUntil eventTwo 15) 14)
+(assert-equals (timeUntil eventThree 17) 21)
